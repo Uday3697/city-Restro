c6331ab03e6b43d0c72cdbc4b99ece6fa15dde3f
uday kumar <udaykumar@inr.group>
2022-08-23 17:20:22 +0530

    chef sec res

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -4,12 +4,26 @@ import { images } from '../../constants';
 import './Chef.css';
 
 const Chef = () => (
-  <div className="app__bg" app__wrapper section__padding>
+  <div className="app__bg app__wrapper section__padding" >
     <div className="app__wrapper_img app__wrapper_img-reverse">
       <img src={images.chef} alt="" />
     </div>
     <div className="app__wrapper_info">
-      <SubHeading title="chef'sa world" />
+      <SubHeading title="chef's world" />
+      <h1 className="headtext__cormorant">What we belive in</h1>
+      <div className="app__chef-content">
+        <div className="app__chef-content_quote">
+          <img src={images.quote} alt="" />
+          <p className="p__opensans">Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto quibusdam voluptatibus qui cum odit, neque culpa provideitatis quo amet? Facere iusto voluptate qui illo quidem?</p>
+          <p className="p__opensans">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maiores maxime tempora, voluptates id ut quidem in officiis consequatur possimus mollitia, cupiditate, cumque ea sint. Mollitia!</p>
+        </div>
+      </div>
+      <div className="app__chef-sign">
+
+        <p>kevin lueo</p>
+        <p className="p__opensa ">Chef and founder</p>
+        <img src={images.sign} alt="" />
+      </div>
     </div>
 
   </div>
